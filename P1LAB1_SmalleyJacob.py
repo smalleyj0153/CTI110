4b8dd2ee7373f9deacd0d47784916a072d176923
smalleyj0153 <smalleyj0153@student.faytechcc.edu>
2024-09-21 22:00:42 -0400

    Add files via upload

---
+++ P1LAB1_SmalleyJacob.py
@@ -0,0 +1,10 @@
+#Jacob Smalley
+#21 Sep 2024
+#P1LAB1
+#First ever Python project
+
+first_name = input('What is your first name?')
+last_name = input('What is your last name?')
+print('Hello,',first_name,last_name,'! Welcome to CTI-110')
+while True:
+    pass
