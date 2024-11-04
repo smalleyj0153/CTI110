c80337b14cdbc8e6b1c69d4d97944ef46b578ccf
smalleyj0153 <smalleyj0153@student.faytechcc.edu>
2024-11-03 21:49:05 -0500

    Add files via upload

---
+++ P4LAB1a_SmalleyJacob.py
@@ -0,0 +1,26 @@
+#Jacob Smalley
+#3 Nov 2024
+#P4LAB1a
+#Square and triangle using turtle.
+
+import turtle
+wn = turtle.Screen()
+jake = turtle.Turtle()
+
+for _ in range(4):
+    jake.forward(100)
+    jake.left(90)
+
+jake.penup()
+jake.forward(150)
+jake.pendown()
+
+for _ in range(3):
+    jake.forward(100)
+    jake.left(120)
+
+#Pseudocode
+#Import turtle
+#assign a name to the commands
+#draw a square
+#draw a triangle
